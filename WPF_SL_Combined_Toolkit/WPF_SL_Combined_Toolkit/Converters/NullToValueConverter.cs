f602338ec6d9a5f8b184936b161cb22f8edcec96
jkuehner <jochen.kuehner@kardex.com>
2015-03-02 10:18:41 +0100

    NET 4.5

---
+++ WPF_SL_Combined_Toolkit/WPF_SL_Combined_Toolkit/Converters/NullToValueConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using WPF_SL_Combined_Toolkit.Converters.Base;
+
+namespace MCC.ControlPanel.SharedClasses.Converters
+{
+    public class NullToValueConverter<T> : ValueConverter
+    {
+        public T NullValue { get; set; }
+        public T NotNullValue { get; set; }
+
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return NullValue;
+            else
+                return NotNullValue;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+
+    public class NullToVisibileConverter : NullToValueConverter<Visibility>
+    {
+        private static readonly Lazy<ValueConverter> _instance = new Lazy<ValueConverter>(() => new NullToVisibileConverter());
+        public static ValueConverter Instance { get { return _instance.Value; } }
+
+        public NullToVisibileConverter()
+        {
+            NullValue = Visibility.Visible;
+            NotNullValue = Visibility.Collapsed;
+        }
+    }
+
+    public class NullToCollapsedConverter : NullToValueConverter<Visibility>
+    {
+        private static readonly Lazy<ValueConverter> _instance = new Lazy<ValueConverter>(() => new NullToCollapsedConverter());
+        public static ValueConverter Instance { get { return _instance.Value; } }
+
+        public NullToCollapsedConverter()
+        {
+            NullValue = Visibility.Collapsed;
+            NotNullValue = Visibility.Visible;
+        }
+    }    
+}

3770192705af7c4277a1a056ebe59c4631ca60c8
jkuehner <jochen.kuehner@kardex.com>
2019-09-26 20:24:44 +0200

    net core 3 build

--- WPF_SL_Combined_Toolkit/WPF_SL_Combined_Toolkit/Properties/AssemblyInfo.cs
+++ WPF_SL_Combined_Toolkit/WPF_SL_Combined_Toolkit/Properties/AssemblyInfo.cs
@@ -7,21 +7,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("WPF_SL_Combined_Toolkit_WPF")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPF_SL_Combined_Toolkit_WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-#if !SILVERLIGHT
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //Speicherort der designspezifischen Ressourcenwörterbücher
     //(wird verwendet, wenn eine Ressource auf der Seite 
@@ -30,20 +15,5 @@ using System.Windows.Markup;
     //(wird verwendet, wenn eine Ressource auf der Seite, in der Anwendung oder einem 
     // designspezifischen Ressourcenwörterbuch nicht gefunden werden kann.)
 )]
-#endif
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
-
 
 [assembly: XmlnsPrefix("urn:wpfsl-combined-toolkit", "cbtk")]
